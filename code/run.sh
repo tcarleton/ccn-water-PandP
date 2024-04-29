40092f022f8204e406d8ea0df330d5ee86c729dd
Levi Crews <levicrews24@gmail.com>
2024-04-29 11:20:20 -0400

    Changed: echo steps so user sees progress

--- code/run.sh
+++ code/run.sh
@@ -5,7 +5,7 @@
 # load necessary modules here (if necessary)
 # module load ...
 
-# python packages
+echo "install python packages"
 pip install numpy
 pip install pandas
 pip install fuzzywuzzy
@@ -14,53 +14,53 @@ pip install openpyxl
 
 # SET WORKING DIRECTORY HERE -----------------------------------------
 
-# insert the path of your working directory after "cd"  
-cd 
+# insert the path of your working directory after "cd"
+cd /scratch/network/lcrews/ccn-water-PandP
 
 # CLEAN VIRTUAL WATER DATA -------------------------------------------
 
-# create country crosswalk
+echo "create country crosswalk"
 python code/1_cleaning/01_country_crosswalk.py
 
-# create crop crosswalk
+echo "create crop crosswalk"
 python code/1_cleaning/02_crop_crosswalk.py
 
-# wrangle trade data from BACI/COMTRADE
+echo "wrangle trade data from BACI/COMTRADE"
 python code/1_cleaning/03_build_tradedata.py
 
-# clean production data from the FAO
+echo "clean production data from the FAO"
 python code/1_cleaning/04_build_proddata.py
 
-# merge trade data
+echo "merge trade data"
 stata -b do code/1_cleaning/05_fill_tradedata.do
 
-# create virtual water flows estimates
+echo "create virtual water flows estimates"
 python code/1_cleaning/06_virtualwater.py
 
-# create global water use estimates per crop
+echo "create global water use estimates per crop"
 stata -b do code/1_cleaning/07_crop_water_use.do
 
-# merge grace and gaez
+echo "merge grace and gaez"
 Rscript code/1_cleaning/08_merge_grace_gaezpy.R
 
-# merge grace and ag employment data
+echo "merge grace and ag employment data"
 Rscript code/1_cleaning/09_merge_grace_agemp.R
 
-# merge grace and virtual water flows
+echo "merge grace and virtual water flows"
 Rscript code/1_cleaning/10_merge_grace_virtwflows.R
 
 # MAKE FIGURES --------------------------------------------------------
 
-# make maps
+echo "make maps"
 Rscript code/2_analysis/01_make_maps.R
 
-# make scatterplots
+echo "make scatterplots"
 stata -b do code/2_analysis/02_make_scatterplots.do
 
-# generate in-text statistics
+echo "generate in-text statistics"
 stata -b do code/2_analysis/03_intext_stats.do
 
-# delete log files
+echo "delete log files"
 rm 02_make_scatterplots.log
 rm 03_intext_stats.log
 rm 05_fill_tradedata.log
